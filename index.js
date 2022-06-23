e8554390c55c3a67d241d8a5564ea5df632f8273
duydang2412 <duydang2412@gmail.com>
2022-06-24 00:05:58 +0700

    refactor(test): use cjs syntax

--- index.js
+++ index.js
@@ -1,61 +1,68 @@
 'use strict';
 
-var postcss = require('postcss');
-var functions = require('postcss-functions');
-var consistent = require('consistent');
-var url = require('url');
+const postcss = require('postcss');
+const functions = require('postcss-functions');
+const consistent = require('consistent');
+const url = require('url');
 
-module.exports = postcss.plugin('prefix', function (options) {
-  options = options || {};
-  var prefix = options.prefix || '';
-  var useUrl = options.useUrl || false;
-  var exclude = options.exclude || null;
+module.exports = function (options) {
+	options = options || {};
+	var prefix = options.prefix || '';
+	var useUrl = options.useUrl || false;
+	var exclude = options.exclude || null;
 
-  if (Array.isArray(prefix)) {
-    if (prefix.length === 1) {
-      prefix = prefix[0];
-    } else if (prefix.length === 0) {
-      prefix = '';
-    }
-  }
+	if (Array.isArray(prefix)) {
+		if (prefix.length === 1) {
+			prefix = prefix[0];
+		} else if (prefix.length === 0) {
+			prefix = '';
+		}
+	}
 
-  var ring;
-  if (Array.isArray(prefix)) {
-    ring = new consistent({
-      members: prefix
-    });
-  }
+	var ring;
+	if (Array.isArray(prefix)) {
+		ring = new consistent({
+			members: prefix,
+		});
+	}
 
-  var getPrefix = function (path) {
-    if (typeof prefix === 'string') {
-      return prefix;
-    }
+	var getPrefix = function (path) {
+		if (typeof prefix === 'string') {
+			return prefix;
+		}
 
-    return ring.get(path);
-  }
+		return ring.get(path);
+	};
 
-  var formatUrl = function (path, includePrefix) {
-    includePrefix = typeof includePrefix !== 'undefined' ? includePrefix : true;
-    var sanitizedPath = path.replace(/['"]/g, '');
+	var formatUrl = function (path, includePrefix) {
+		includePrefix =
+			typeof includePrefix !== 'undefined' ? includePrefix : true;
+		var sanitizedPath = path.replace(/['"]/g, '');
 
-    if ((exclude && exclude.test(path)) || /^([a-z]+:\/\/|\/\/)/i.test(path)) {
-      includePrefix = false;
-    }
+		if (
+			(exclude && exclude.test(path)) ||
+			/^([a-z]+:\/\/|\/\/)/i.test(path)
+		) {
+			includePrefix = false;
+		}
 
-    var prefix = includePrefix ? getPrefix(sanitizedPath) : '';
+		var prefix = includePrefix ? getPrefix(sanitizedPath) : '';
 
-    return 'url('+url.resolve(prefix, sanitizedPath)+')';
-  };
+		return 'url(' + url.resolve(prefix, sanitizedPath) + ')';
+	};
 
-  return postcss().use(functions({
-    functions: {
-      cdn: function(path) {
-        return formatUrl(path);
-      },
+	return postcss().use(
+		functions({
+			functions: {
+				cdn: function (path) {
+					return formatUrl(path);
+				},
 
-      url: function(path) {
-        return formatUrl(path, useUrl);
-      }
-    }
-  }));
-});
+				url: function (path) {
+					return formatUrl(path, useUrl);
+				},
+			},
+		})
+	);
+};
+module.exports.postcss = true;
--- test/test.js
+++ test/test.js
@@ -1,78 +1,90 @@
-import test from 'ava';
-import fs from 'fs';
-import path from 'path';
-import postcss from 'postcss';
-import plugin from '../';
+const test = require('ava');
+const fs = require('fs');
+const path = require('path');
+const postcss = require('postcss');
+const plugin = require('../index.js');
 
 function readFile(file) {
-  return new Promise((resolve) => {
-    fs.readFile(file, (err, buffer) => {
-      resolve(buffer.toString('utf-8'));
-    });
-  });
+	return new Promise((resolve) => {
+		fs.readFile(file, (err, buffer) => {
+			resolve(buffer.toString('utf-8'));
+		});
+	});
 }
 
 async function loadFiles(name) {
-  return {
-    before: await readFile(path.resolve(__dirname, 'fixtures', `${name}.css`)),
-    after: await readFile(path.resolve(__dirname, 'fixtures', `${name}.out.css`))
-  }
+	return {
+		before: await readFile(
+			path.resolve(__dirname, 'fixtures', `${name}.css`)
+		),
+		after: await readFile(
+			path.resolve(__dirname, 'fixtures', `${name}.out.css`)
+		),
+	};
 }
 
 async function testFile(t, name, opts) {
-  let files = await loadFiles(name);
+	let files = await loadFiles(name);
 
-  const res = await postcss(plugin(opts)).process(files.before);
+	const res = await postcss(plugin(opts)).process(files.before);
 
-  t.is(res.css, files.after);
+	t.is(res.css, files.after);
 }
 
 test('prefix as string', (t) => {
-  return testFile(t, 'simple', {
-    prefix: 'https://img1.example.com'
-  });
+	return testFile(t, 'simple', {
+		prefix: 'https://img1.example.com',
+	});
 });
 
 test('prefix as array', (t) => {
-  return testFile(t, 'simple', {
-    prefix: ['https://img1.example.com']
-  });
+	return testFile(t, 'simple', {
+		prefix: ['https://img1.example.com'],
+	});
 });
 
 test('multiple prefix urls', (t) => {
-  return testFile(t, 'multiple', {
-    prefix: ['https://img1.example.com', 'https://img2.example.com', 'https://img3.example.com']
-  });
+	return testFile(t, 'multiple', {
+		prefix: [
+			'https://img1.example.com',
+			'https://img2.example.com',
+			'https://img3.example.com',
+		],
+	});
 });
 
 test('using url', (t) => {
-  return testFile(t, 'url', {
-    prefix: ['https://img1.example.com', 'https://img2.example.com', 'https://img3.example.com'],
-    useUrl: true
-  });
+	return testFile(t, 'url', {
+		prefix: [
+			'https://img1.example.com',
+			'https://img2.example.com',
+			'https://img3.example.com',
+		],
+		useUrl: true,
+	});
 });
 
 test('exclude absolute urls', (t) => {
-  return testFile(t, 'absolute', {
-    prefix: ['https://img1.example.com', 'https://img2.example.com']
-  });
+	return testFile(t, 'absolute', {
+		prefix: ['https://img1.example.com', 'https://img2.example.com'],
+	});
 });
 
 test('exclude pattern', (t) => {
-  return testFile(t, 'exclude', {
-    prefix: ['https://img1.example.com', 'https://img2.example.com'],
-    exclude: /\/exclude-this\//
-  });
+	return testFile(t, 'exclude', {
+		prefix: ['https://img1.example.com', 'https://img2.example.com'],
+		exclude: /\/exclude-this\//,
+	});
 });
 
 test('the mother load', (t) => {
-  return testFile(t, 'full', {
-    prefix: ['https://img1.example.com', 'https://img2.example.com'],
-    exclude: /\/exclude-this\//,
-    useUrl: true
-  });
+	return testFile(t, 'full', {
+		prefix: ['https://img1.example.com', 'https://img2.example.com'],
+		exclude: /\/exclude-this\//,
+		useUrl: true,
+	});
 });
 
 test('no options', (t) => {
-  return testFile(t, 'nada');
+	return testFile(t, 'nada');
 });
